3e5ebc81549d7cf7d800e7ee861d1e8d0220cbc0
juhongahn <juhong.ahn.dev@gmail.com>
2023-11-02 19:27:50 +0900

    feat: 지정한 ref 외에 클릭할 시 지정한 ref를 닫는 훅 구현

---
+++ src/hooks/useOnClickOutside.ts
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+const useOnClickOutside = (ref:React.RefObject<HTMLElement>, callback: ()=> void) => {
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      callback();
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, callback]);
+};
+
+export default useOnClickOutside;
