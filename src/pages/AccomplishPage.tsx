bc749e9f5ab32dbe6a226223905b2befcaa4de68
juhongahn <juhong.ahn.dev@gmail.com>
2023-11-03 00:41:06 +0900

    feat: 페이지에 title 추가

--- src/pages/AccomplishPage.tsx
+++ src/pages/AccomplishPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import AccomplishBoard from "@/components/board/AccomplishBoard";
@@ -5,6 +6,9 @@ import AccomplishBoard from "@/components/board/AccomplishBoard";
 const queryClient = new QueryClient();
 
 const AccomplishPage = () => {
+  useEffect(() => {
+    document.title = "My Accomplishment";
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <AccomplishBoard />
